f7e15617899c9a987144bbb300768735c2c64159
ravi00006 <66362202+ravi00006@users.noreply.github.com>
2022-01-26 21:31:40 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,15 @@
+const myvar4 = 6 > 10; //false
+
+const myvar3 = 1 < 10; //true
+
+const myvar2 = 12==12;  //true
+
+const myvar1 = 9==10;  //false
+
+console.log(myvar1);
+
+console.log(myvar2);
+
+console.log(myvar3);
+
+console.log(myvar4);
